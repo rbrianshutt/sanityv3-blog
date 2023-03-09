9a3bb63464307c74aedbae2eb9e0e587959d2a48
rbrianshutt <rbrianshutt@gmai.com>
2023-03-09 12:01:29 -0600

    pic

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
 	return (
 		<header className='flex items-center justify-between space-x-2 font-bold px-10 py-5'>
 			<div className='flex items-center space-x-2'>
-				{/* <Link href='/'>
+				<Link href='/'>
 					<Image
 						className='rounded full'
 						src='/../public/profile-pic.jpg'
@@ -13,7 +13,7 @@ function Header() {
 						height={50}
 						alt='logo'
 					/>
-				</Link> */}
+				</Link>
 				<h1>Brian Shutt</h1>
 			</div>
 			<div>
