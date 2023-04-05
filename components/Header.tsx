e619e8d5bef864dd4666154764cd22e6e3319324
rbrianshutt <rbrianshutt@gmai.com>
2023-04-05 10:17:32 -0500

    added usericon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,21 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import {UserIcon} from '@heroicons/react/24/solid'
 
 function Header() {
 	return (
 		<header className='flex items-center justify-between space-x-2 font-bold px-10 py-5'>
 			<div className='flex items-center space-x-2'>
 				<Link href='/'>
-					<Image
+					<UserIcon className=' w-10 h-10' />
+					{/* <Image
 						className='rounded full'
 						src='/../public/profile-pic.jpg'
 						width={50}
 						height={50}
 						alt='logo'
-					/>
+					/> */}
+					
 				</Link>
 				<h1>Brian Shutt</h1>
 			</div>
